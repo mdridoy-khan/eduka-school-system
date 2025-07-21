045f7ef763ad74fbf01b8cda09f370a688a707a1
Md Majedul Islam <mdridoy9902@gmail.com>
2025-07-21 15:34:29 +0600

    project files ready to work header component created

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <div className="container" style={{ backgroundColor: 'var(--primary-color)' }}>
+            This is header
+        </div>
+    )
+}
+export default Header
